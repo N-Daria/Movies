28b7c3d4167be5a9c68126f84fbe41ad92cc7223
Daria Nikulina <daryanickulina@yandex.ru>
2022-11-09 18:00:01 +0400

    fix: regexURL

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,4 @@
-const regExUrl = /^(http(s)?:\/{2})((w{3}\.)?)(([\w-]{1,})(\.))+([\w\-\/\.~:?#\[\]@!$&'\(\)*\+,;=]+)()?([\w])?$(#)?/im;
+const regExUrl = /^(http(s)?:\/{2})((w{3}\.)?)(([\w-]{1,})(\.))+([\w\-.~:?#[\]@!$&'()*+,;=]+)()?([\w])?$(#)?/im;
 const regExEn = /[\w\W][^а-я]/mi;
 const regExRu = /[\W\d][^a-z]/mi;
 const allowedCorsUrl = [
