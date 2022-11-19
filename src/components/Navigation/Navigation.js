adc9e862efd8b6a2f666208a3d3c2ddcb810eb3c
Daria Nikulina <daryanickulina@yandex.ru>
2022-11-19 16:51:03 +0400

    feat: adds Navigation

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,42 @@
+import './Navigation.css';
+import { useState } from "react";
+
+export default function Navigation(props) {
+  let navigationClass = props.loggedIn ? "navigation" : "navigation_none";
+  let navigationButtonClass = props.loggedIn ? "navigation_button" : "navigation_none";
+
+  const [navButton, setnavButton] = useState("navigation__button_open");
+  const [navHide, setnavHide] = useState("navigation_hide");
+
+  function hideNav() {
+    navButton === "navigation__button_open"
+      ? setnavButton("navigation__button_close")
+      : setnavButton("navigation__button_open");
+
+    navHide === "navigation_open"
+      ? setnavHide("navigation_hide")
+      : setnavHide("navigation_open");
+  }
+
+  return (
+    <>
+      <button className={`${navButton} ${navigationButtonClass}`} onClick={hideNav} />
+      <nav className={`${navigationClass} ${navHide}`}>
+        <ul className="navigation_list list">
+          <li className="navigation__link navigation__link_hide">
+            <a href="/" className="navigation__link-text link">Главная</a>
+          </li>
+          <li className="navigation__link">
+            <a href="/movies" className="navigation__link-text link">Фильмы</a>
+          </li>
+          <li className="navigation__link">
+            <a href="/movies" className="navigation__link-text link">Сохранённые фильмы</a>
+          </li>
+        </ul>
+        <button className="navigation__account-button">
+          <a href="/users/me" className="link navigation__account-text">Аккаунт</a>
+        </button>
+      </nav>
+    </>
+  )
+}
