986c0cac7e5dca86854746437b11f11e247eebd5
Daria Nikulina <daryanickulina@yandex.ru>
2022-11-24 22:06:34 +0400

    fix: Authorization props

--- src/components/Authorization/Authorization.js
+++ src/components/Authorization/Authorization.js
@@ -11,15 +11,14 @@ export default function Authorization(props) {
   }
 
   return (
-
     <section className={`authorization`}>
       <img src={logo} alt='логотип' className="authorization__logo" />
       <h2 className='authorization__header'>{props.header}</h2>
-      <form name={props.formName} onSubmit={handleSubmit}>
+      <form name={props.formName} onSubmit={handleSubmit} id={props.formName}>
 
         {props.children}
 
-        <button className="authorization__button button" type="submit">{props.buttonText}</button>
+        <button className="authorization__button authorization__button_disabled button" type="submit">{props.buttonText}</button>
         <a className='authorization__link link' href={props.redirect}>{props.linktext}
           <span className='authorization__link-span'>{props.redirectText}</span>
         </a>
