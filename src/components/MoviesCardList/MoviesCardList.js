22d058a2064c3d29c18f0ef1df9592d3e3d5a2f9
Daria Nikulina <daryanickulina@yandex.ru>
2022-11-26 20:00:34 +0400

    feat: adds MoviesCardList

---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,121 @@
+import './MoviesCardList.css';
+import MoviesCard from '../MoviesCard/MoviesCard';
+import image from '../../images/image.png';
+import Preloader from '../Preloader/Preloader';
+
+export default function MoviesCardList(props) {
+
+  let buttonClass = props.url === '/movies' ? 'content__open button' : 'content_none';
+
+  return (
+    <main className='content'>
+      <ul className='content__list list'>
+
+        {/* 320 _______________________________________________ */}
+
+        <MoviesCard
+          movieName='33 слова о дизайне'
+          movieImg={image}
+          movieDuration='1ч42м'
+        />
+
+        {
+          props.card ?
+            <MoviesCard
+              movieName='33 слова о дизайне'
+              movieImg={image}
+              movieDuration='1ч42м'
+            />
+            : <Preloader />
+        }
+
+        <MoviesCard
+          movieName='33 слова о дизайне'
+          movieImg={image}
+          movieDuration='1ч42м'
+        />
+        <MoviesCard
+          movieName='33 слова о дизайне'
+          movieImg={image}
+          movieDuration='1ч42м'
+        />
+        <MoviesCard
+          movieName='33 слова о дизайне'
+          movieImg={image}
+          movieDuration='1ч42м'
+        />
+
+        {/* 768 _______________________________________________ */}
+
+        {/* <MoviesCard
+          movieName='33 слова о дизайне'
+          movieImg={image}
+          movieDuration='1ч42м'
+        />
+        <MoviesCard
+          movieName='33 слова о дизайне'
+          movieImg={image}
+          movieDuration='1ч42м'
+        />
+        <MoviesCard
+          movieName='33 слова о дизайне'
+          movieImg={image}
+          movieDuration='1ч42м'
+        /> */}
+
+        {/* 1280 _______________________________________________ */}
+
+        {/*   
+      <MoviesCard
+        movieName='33 слова о дизайне'
+        movieImg={image}
+        movieDuration='1ч42м'
+      />
+      <MoviesCard
+        movieName='33 слова о дизайне'
+        movieImg={image}
+        movieDuration='1ч42м'
+      />
+      <MoviesCard
+        movieName='33 слова о дизайне'
+        movieImg={image}
+        movieDuration='1ч42м'
+      />
+      <MoviesCard
+        movieName='33 слова о дизайне'
+        movieImg={image}
+        movieDuration='1ч42м'
+      />
+
+      <MoviesCard
+        movieName='33 слова о дизайне'
+        movieImg={image}
+        movieDuration='1ч42м'
+      />
+
+      <MoviesCard
+        movieName='33 слова о дизайне'
+        movieImg={image}
+        movieDuration='1ч42м'
+      />
+
+      <MoviesCard
+        movieName='33 слова о дизайне'
+        movieImg={image}
+        movieDuration='1ч42м'
+      />
+
+      <MoviesCard
+        movieName='33 слова о дизайне'
+        movieImg={image}
+        movieDuration='1ч42м'
+      /> 
+      */}
+
+      </ul>
+
+      <button type='button' className={buttonClass}>еще</button>
+
+    </main >
+  )
+};
