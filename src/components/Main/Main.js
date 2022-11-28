b0ed7671ff4f478f06d9067eda8045faff5a787d
Daria Nikulina <daryanickulina@yandex.ru>
2022-11-28 22:07:35 +0400

    feat: adds Main

---
+++ src/components/Main/Main.js
@@ -0,0 +1,19 @@
+import './Main.css';
+import AboutProject from '../AboutProject/AboutProject';
+import Promo from '../Promo/Promo';
+import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
+
+export default function Main(props) {
+
+  return (
+    <>
+      <Promo />
+      <AboutProject />
+      <Techs />
+      <AboutMe />
+      <Portfolio />
+    </>
+  )
+};
