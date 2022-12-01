783885afe95feb3b1944374ee6cf374d746a64bd
Daria Nikulina <daryanickulina@yandex.ru>
2022-12-01 14:42:57 +0400

    feat: adds SearchForm validation

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,21 +1,51 @@
 import './SearchForm.css';
+import React from 'react';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
 export default function SearchForm(props) {
+  const [movieName, setmovieName] = React.useState("");
+  const [errorText, setErrorText] = React.useState("");
 
-  function handleSubmit() {
+  function inputChange(event) {
+    getInputValue(event);
+    isValid(event.target)
+  }
 
+  function getInputValue(event) {
+    if (event.target.name === 'search-input') {
+      setmovieName(`${event.target.value}`);
+    }
+  }
+
+  function isValid(formInput) {
+    if (formInput.validity.valid) {
+      setErrorText('');
+      return true
+    } else {
+      setErrorText('search__error_open');
+      return false
+    }
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (isValid(event.target.movieName)) {
+
+    }
   }
 
   return (
-    <form className='search' name={props.formName} onSubmit={handleSubmit} id='search'>
+    <form noValidate className='search' name={props.formName} onSubmit={handleSubmit} id='search'>
+      <span className={`${errorText} search__error`}>Нужно ввести ключевое слово</span>
       <input
         id="search-input"
-        name="search-input"
+        name="movieName"
         className="search__input"
         placeholder="Фильм"
         required
         type="text"
+        onChange={inputChange}
       />
       <button type='submit' className='search__button button' />
 
