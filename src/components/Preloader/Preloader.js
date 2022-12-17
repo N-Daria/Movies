2d9508719399d0a3d97f111b5c7db6af2d3a2c68
Daria Nikulina <daryanickulina@yandex.ru>
2022-12-17 20:50:43 +0400

    fix: Preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,8 +1,10 @@
 import './Preloader.css'
 
-export default function Preloader() {
+export default function Preloader(props) {
+  const preloaderClass = props.preloaderBlock ? 'preloader_open' : 'preloader_hide';
+
   return (
-    <div className="preloader">
+    <div className={`preloader ${preloaderClass}`}>
       <div className="preloader__container">
         <span className="preloader__round"></span>
       </div>
