a22ddbcc8f4e09c12f0af40f938ad971485de991
Daria Nikulina <daryanickulina@yandex.ru>
2022-12-18 18:52:46 +0400

    feat: adds cards render

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
+import React from 'react';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
@@ -10,8 +11,90 @@ import Profile from '../Profile/Profile';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Main from '../Main/Main';
+import { getMovieList } from '../../utils/MoviesApi';
+import Error from '../Error/Error';
+import Preloader from '../Preloader/Preloader';
 
-export default function App() {
+export default React.memo(function App() {
+  const [loggedIn, setloggedIn] = React.useState(false);
+  const [beatFilmsList, setBeatFilmsList] = React.useState([]);
+  const [filteredList, setFilteredList] = React.useState([]);
+
+  const [preloaderBlock, setPreloaderBlock] = React.useState(false);
+  const [moviesBlock, setMoviesBlock] = React.useState(false);
+  const [errorBlock, setErrorBlock] = React.useState(false);
+  const [errorText, setErrorText] = React.useState('');
+
+  const [isShortMovie, setIsShortMovie] = React.useState(false);
+  const [searchWord, setSearchWord] = React.useState('');
+
+  // const toggleErrorBlock = React.useCallback((state) => {
+  //   setErrorBlock(state)
+  // }, [moviesBlock])
+
+  function changeSearchWord(state) {
+    setSearchWord(state);
+  }
+
+  function togglePreloaderBlock(state) {
+    setPreloaderBlock(state);
+  }
+
+  function toggleMoviesBlock(state) {
+    setMoviesBlock(state);
+  }
+
+  function toggleErrorBlock(state) {
+    setErrorBlock(state)
+  }
+
+  function changeErrorText(text) {
+    setErrorText(text)
+  }
+
+  // const movieList = React.useMemo(() => setFilteredList(filteredList), [isShortMovie]);
+
+  function filterMovies(movieName, allMovies) {
+    setFilteredList([]);
+
+    if (!allMovies) {
+      return null
+    }
+
+    for (let i = 0; i < allMovies.length; i++) {
+      if ((allMovies[i].country).toLowerCase().includes(movieName) ||
+        (allMovies[i].description).toLowerCase().includes(movieName) ||
+        (allMovies[i].director).toLowerCase().includes(movieName) ||
+        (allMovies[i].nameEN).toLowerCase().includes(movieName) ||
+        (allMovies[i].nameRU).toLowerCase().includes(movieName) ||
+        (allMovies[i].year).toLowerCase().includes(movieName)) {
+        setFilteredList(list => [...list, allMovies[i]]);
+      }
+    }
+  };
+
+  // const changeMovieList = React.useEffect(() => {
+  // }, [loggedIn])
+
+  function getBeatFilms() {
+    localStorage.clear();
+
+    return getMovieList()
+      .then((res) => {
+        localStorage.setItem('movies', JSON.stringify(res));
+        localStorage.setItem('isShortMovie', isShortMovie);
+        localStorage.setItem('searchWord', searchWord);
+
+        setBeatFilmsList(res);
+        filterMovies(searchWord, JSON.parse(localStorage.movies));
+      })
+      .catch((err) => {
+        toggleMoviesBlock(false);
+        togglePreloaderBlock(false);
+        toggleErrorBlock(true);
+        changeErrorText('Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз');
+      })
+  };
 
   return (
     <Routes>
@@ -19,13 +102,58 @@ export default function App() {
       <Route path='/signin' element={<Login />} />
       <Route path='/signup' element={<Register />} />
 
-      <Route path='/movies' element={<> <Header loggedIn='true' /> < Movies /> <Footer /> </>} />
-      <Route path='/saved-movies' element={<> <Header loggedIn='true' /> < SavedMovies /> <Footer /> </>} />
-      <Route path='/profile' element={<> <Header loggedIn='true' /> <Profile /> <Footer /> </>} />
-      <Route path='/' element={<> <Header loggedIn='true' />< Main /><Footer /> </>} />
+      <Route path='/movies' element={<>
+        <Header loggedIn={loggedIn} />
+
+        < Movies
+          filteredList={filteredList}
+          getBeatFilms={getBeatFilms}
+
+          togglePreloaderBlock={togglePreloaderBlock}
+          filterMovies={filterMovies}
+          searchWord={searchWord}
+
+          toggleMoviesBlock={toggleMoviesBlock}
+          toggleErrorBlock={toggleErrorBlock}
+          changeErrorText={changeErrorText}
+
+          changeSearchWord={changeSearchWord}
+          moviesBlock={moviesBlock}
+        />
+
+        <Preloader
+          preloaderBlock={preloaderBlock}
+        />
+
+        <Error
+          errorBlock={errorBlock}
+          errorText={errorText}
+        />
+
+        <Footer />
+      </>} />
+
+      <Route path='/saved-movies' element={<>
+        <Header loggedIn={loggedIn} />
+        < SavedMovies />
+        <Footer />
+      </>} />
+
+      <Route path='/profile' element={<>
+        <Header loggedIn={loggedIn} />
+        <Profile />
+        <Footer />
+      </>} />
+
+      <Route path='/' element={<>
+        <Header loggedIn={loggedIn} />
+        < Main />
+        <Footer />
+      </>} />
 
       <Route path='*' element={<Undefined />} />
 
     </Routes>
   );
-};
+}
+);
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,95 @@
+import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import React from 'react';
 
-export default function Movies(props) {
+export default React.memo(function Movies(props) {
+  const buttonClass = window.location.pathname === '/movies' && props.moviesBlock ? 'content__button_open button' : 'content_none';
+  const moviesClass = props.moviesBlock ? 'content_open' : 'content_none';
+
+  const [renderedCards, setRenderedCards] = React.useState([]);
+
+  const [cardsNumberOnClick, setCardsNumberOnClick] = React.useState(0);
+  const [cardsNumber, setCardsNumber] = React.useState(0);
+  const [currentCardsNumber, setCurrentCardsNumber] = React.useState(0);
+
+  function getScreenWidth() {
+    if (window.innerWidth <= 577) {
+      setCardsNumber(5);
+      setCardsNumberOnClick(2);
+    } else if (window.innerWidth <= 930) {
+      setCardsNumber(8);
+      setCardsNumberOnClick(2);
+    } else if (window.innerWidth <= 1280) {
+      setCardsNumber(9);
+      setCardsNumberOnClick(3);
+    } else {
+      setCardsNumber(12);
+      setCardsNumberOnClick(4);
+    }
+
+    setCurrentCardsNumber(cardsNumber);
+  }
+
+  React.useEffect(() => {
+    getScreenWidth();
+
+    if (props.filteredList.length > cardsNumber) {
+      setRenderedCards(props.filteredList.slice(0, cardsNumber));
+    } else {
+      setRenderedCards(props.filteredList.slice(0));
+    }
+  }, [props.filteredList]);
+
+  function openMoreCards() {
+    setRenderedCards([...renderedCards.concat(props.filteredList.slice(currentCardsNumber, currentCardsNumber + cardsNumberOnClick))]);
+    setCurrentCardsNumber(currentCardsNumber + cardsNumberOnClick);
+  }
+
+  function toggleContent() {
+    props.togglePreloaderBlock(true);
+
+    if (localStorage.getItem('movies')) {
+      props.filterMovies(props.searchWord, JSON.parse(localStorage.movies));
+      checkSearchError();
+    }
+    else {
+      props.getBeatFilms()
+        .then((res) => {
+          checkSearchError();
+        })
+        .catch(console.log)
+    }
+  }
+
+  function checkSearchError() {
+    if (props.filteredList.length >= 1) {
+      props.toggleMoviesBlock(true);
+      props.toggleErrorBlock(false);
+    } else {
+      props.toggleErrorBlock(true);
+      props.changeErrorText('Ничего не найдено');
+    }
+
+    props.togglePreloaderBlock(false);
+  }
 
   return (
-    <main>
-      <SearchForm />
-      <MoviesCardList url='/movies' />
-    </main>
+    <>
+      <SearchForm
+        toggleContent={toggleContent}
+        changeSearchWord={props.changeSearchWord}
+      />
+
+      <main className={`${moviesClass} content`}>
+
+        < MoviesCardList
+          renderedCards={renderedCards}
+        />
+
+        <button type='button' className={`${buttonClass} content__button`} onClick={openMoreCards}>еще</button>
+      </main>
+    </>
   )
-};
+}
+);
