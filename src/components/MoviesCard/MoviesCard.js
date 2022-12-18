4628a32203bc0ffd337c3b1d66030d71b9a2a5f6
Daria Nikulina <daryanickulina@yandex.ru>
2022-12-18 18:52:10 +0400

    fix: MoviesCard && style

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,23 +1,24 @@
 import './MoviesCard.css';
 import CardCheckbox from '../CardCheckbox/CardCheckbox';
+import React from 'react';
 
-export default function MoviesCard(props) {
+export default React.memo(function MoviesCard(props) {
 
   return (
     <li className='card'>
       <figure className='card__card-block'>
-        <img className='card__image' src={props.movieImg} alt={props.movieName} />
+        <img className='card__image' src={`https://api.nomoreparties.co/${props.image.url}`} alt={props.nameRU} />
         <figcaption className='card__info'>
-          <h3 className='card__header'>{props.movieName}</h3>
+          <h3 className='card__header'>{props.nameRU}</h3>
 
           {props.url === "/saved-movies" ?
             <button className='card__delete button' type='button' />
             : <CardCheckbox />
           }
 
-          <p className='card__duration'>{props.movieDuration}</p>
+          <p className='card__duration'>{`${Math.floor(props.duration / 60)}ч ${Math.floor(props.duration - props.duration / 60)}м`}</p>
         </figcaption>
       </figure>
     </li>
   )
-};
+})
