18d5e450edff260be71d950d3ee464cd430c735b
Daria Nikulina <daryanickulina@yandex.ru>
2022-12-26 20:13:24 +0400

    refactor: card like style

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,19 +1,28 @@
 import './MoviesCard.css';
-import CardCheckbox from '../CardCheckbox/CardCheckbox';
 import React from 'react';
 
 export default React.memo(function MoviesCard(props) {
+  const buttonType = window.location.pathname === "/saved-movies" ? true : false;
+
+  const [likeClass, setLikeClass] = React.useState(props.isLiked);
+
+  function changeCardLike() {
+    props.likeCard();
+    likeClass ? setLikeClass('') : setLikeClass('card__like_active');
+  }
 
   return (
     <li className='card'>
       <figure className='card__card-block'>
-        <img className='card__image' src={`https://api.nomoreparties.co/${props.image.url}`} alt={props.nameRU} />
+        <a href={props.trailerLink} className='link'>
+          <img className='card__image' src={`https://api.nomoreparties.co/${props.image.url}`} alt={props.nameRU} />
+        </a>
         <figcaption className='card__info'>
           <h3 className='card__header'>{props.nameRU}</h3>
 
-          {props.url === "/saved-movies" ?
+          {buttonType ?
             <button className='card__delete button' type='button' />
-            : <CardCheckbox />
+            : <button type='button' className={`card__like button ${likeClass}`} onClick={changeCardLike} />
           }
 
           <p className='card__duration'>{`${Math.floor(props.duration / 60)}ч ${Math.floor(props.duration - props.duration / 60)}м`}</p>
