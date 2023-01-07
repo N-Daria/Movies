60b822c93a48897d693bd841eab176151598701c
Daria Nikulina <daryanickulina@yandex.ru>
2023-01-07 21:43:05 +0400

    fix: cors allowed url

--- utils/utils.js
+++ utils/utils.js
@@ -1,11 +1,6 @@
 const regExEn = /[\w\W][^а-я]/mi;
 const regExRu = /[\W\d][^a-z]/mi;
-const allowedCorsUrl = [
-  'http://localhost:3001',
-  'https://localhost:3001',
-  'http://localhost:3002',
-  'https://localhost:3002',
-];
+const allowedCorsUrl = ['http://localhost:3000'];
 
 module.exports = {
   regExEn,
