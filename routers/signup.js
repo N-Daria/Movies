54cf726b52f327fc7bf503b25bc0816ec5621a81
Daria Nikulina <daryanickulina@yandex.ru>
2023-01-07 21:40:34 +0400

    fix: signup celebrate validation

--- routers/signup.js
+++ routers/signup.js
@@ -5,7 +5,10 @@ const { createUser } = require('../controllers/auth');
 signupRouter.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
-    email: Joi.string().required().min(2).email(),
+    email: Joi.string().required().min(2).email()
+      .messages({
+        'string.email': 'Поле должно соответствовать типу email',
+      }),
     password: Joi.string().required(),
   }),
 }), createUser);
