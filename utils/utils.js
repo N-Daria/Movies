951ec4e6829c629e5585db6da3670a031ed63bd3
Daria Nikulina <daryanickulina@yandex.ru>
2023-01-11 14:47:11 +0400

    fix: allowedCorsUrl

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,10 @@
-const regExEn = /[\w\W][^а-я]/mi;
-const regExRu = /[\W\d][^a-z]/mi;
-const allowedCorsUrl = ['http://localhost:3000'];
+const regExEn = /[\w\W\s][^а-я]+/mi;
+const regExRu = /[а-яА-ЯёЁ\s\w]+/mi;
+const allowedCorsUrl = [
+  'http://localhost:3000',
+  'http://movies.daria.nomoredomains.icu',
+  'https://movies.daria.nomoredomains.icu',
+];
 
 module.exports = {
   regExEn,
